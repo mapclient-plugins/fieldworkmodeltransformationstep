64b9020f997ed83b8e52e01d28fa1a13a370391a
Ju Zhang <ju@tomahawk.(none)>
2014-01-13 17:06:06 +1300

    fixed typo in GF.transformRigidScaleAboutCoM

--- fieldworkmodeltransformationstep/step.py
+++ fieldworkmodeltransformationstep/step.py
@@ -44,7 +44,7 @@ class fieldworkmodeltransformationStep(WorkflowStepMountPoint):
         # Put your execute step code here before calling the '_doneExecution' method.
         GFTransforms = {'affine': self.GF.transformAffine,
                         'rigid':  self.GF.transformRigidRotateAboutCoM,
-                        'rigidScale': self.GF.transformRigidScale3DAboutCoM,
+                        'rigidScale': self.GF.transformRigidScaleAboutCoM,
                         }
         try:
             transformFunction = GFTransforms[self.T.transformType]
